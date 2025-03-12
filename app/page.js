3901cbcc1850e4c9a7c0c0baefe8c61f8b72cd76
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-12 19:01:47 -0400

    done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // app/page.js
 export default function Home() {
   return (
-    <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden shadow-lg">
+    <div className="w-[1769px] h-[1211px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden shadow-lg">
       <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
         <div className="w-[150px] h-[52px] left-[4px] top-[8px] absolute bg-[#000000] opacity-20 rounded-[49px]" />
         <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
@@ -13,12 +13,12 @@ export default function Home() {
       </div>
       <div className="w-[680px] left-[143px] top-[500px] absolute justify-start text-[#eaecd7] text-[110px] font-black font-['Inknut_Antiqua'] leading-[134.20px]">Bla Bla Coffee!</div>
       <div className="w-[680px] left-[148px] top-[740px] absolute justify-start text-[#eaecd7] text-[40px] font-semibold font-['Inknut_Antiqua'] leading-[48.80px]">Your NO.1 Choice </div>
-      <div className="left-[270px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Menu</div>
+      <div className="left-[370px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Menu</div>
       <div className="left-[64px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Home</div>
-      <div className="left-[476px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Login</div>
+      <div className="left-[776px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Login</div>
       <div className="w-full h-[102px] left-0 top-[1049px] absolute bg-white flex justify-around items-center"></div>
 
-      <div className="w-full h-[80px] left-0 top-[1049px] absolute bg-white flex justify-around items-center">
+      <div className="w-full h-[60px] left-0 top-[1049px] absolute bg-white flex justify-around items-center">
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Contact Information</div>
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Company Policy</div>
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Be our partner today</div>
