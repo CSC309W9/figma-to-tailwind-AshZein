0f2d66951c908fca27e58c8b78a1767faf3eb14c
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-12 18:59:32 -0400

    drop shadow

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden shadow-lg">
       <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
-        <div className="w-[150px] h-[52px] left-[3px] top-[5px] absolute bg-[#000000] opacity-20 rounded-[49px]" />
+        <div className="w-[150px] h-[52px] left-[4px] top-[8px] absolute bg-[#000000] opacity-20 rounded-[49px]" />
         <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
         <div className="left-[50%] top-[50%] absolute transform -translate-x-1/2 -translate-y-1/2 justify-center text-[#db4439] text-base font-black font-['Inknut_Antiqua']">Click</div>
       </div>
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="left-[476px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Login</div>
       <div className="w-full h-[102px] left-0 top-[1049px] absolute bg-white flex justify-around items-center"></div>
 
-      <div className="w-full h-[102px] left-0 top-[1049px] absolute bg-white flex justify-around items-center">
+      <div className="w-full h-[80px] left-0 top-[1049px] absolute bg-white flex justify-around items-center">
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Contact Information</div>
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Company Policy</div>
         <div className="justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">Be our partner today</div>
